4cf694e69bfa37c82132a1015299b884cc03e0c2
roywongx <roywangivy@hotmail.com>
2026-06-13 12:58:37 +0800

    Remove temp file

--- count_stats.py
+++
@@ -1,7 +0,0 @@
-import re
-with open('index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-lines = content.count('\n')
-print(f'Lines: {lines}')
-funcs = re.findall(r'function (\w+)\(', content)
-print(f'Functions: {len(funcs)}')
